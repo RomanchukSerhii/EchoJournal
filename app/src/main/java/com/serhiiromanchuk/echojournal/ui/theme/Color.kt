1a56a83e4389b85e9ef97701daa4294f86ccd9bf
RomanchukSerhii <serhiiromanchuk@gmail.com>
2025-01-01 12:59:37 +0200

    Set theme

--- app/src/main/java/com/serhiiromanchuk/echojournal/ui/theme/Color.kt
+++ app/src/main/java/com/serhiiromanchuk/echojournal/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.serhiiromanchuk.echojournal.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val EchoLightBlue = Color(0xFFF2F5FF)
+val EchoBlue = Color(0xFF00419C)
+val EchoDark = Color(0xFF191A20)
+val EchoGrayBlue = Color(0xFF40434F)
+val EchoDarkSteel = Color(0xFF3B4663 )
+val EchoMutedGray = Color(0xFF6C7085 )
+val EchoLightGray = Color(0xFFC1C3CE )
--- app/src/main/java/com/serhiiromanchuk/echojournal/ui/theme/Type.kt
+++ app/src/main/java/com/serhiiromanchuk/echojournal/ui/theme/Type.kt
@@ -2,33 +2,64 @@ package com.serhiiromanchuk.echojournal.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.serhiiromanchuk.echojournal.R
+
+val Inter = FontFamily(
+    Font(
+        resId = R.font.inter_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.inter_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.inter_semibold,
+        weight = FontWeight.SemiBold
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    labelMedium = TextStyle(
+        fontFamily = Inter,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 12.sp,
+        color = EchoGrayBlue
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodySmall = TextStyle(
+        fontFamily = Inter,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontSize = 13.sp,
+        color = EchoGrayBlue
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        color = EchoGrayBlue
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        color = EchoDark
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 22.sp,
+        color = EchoDark
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 26.sp,
+        color = EchoDark
+    ),
 )
